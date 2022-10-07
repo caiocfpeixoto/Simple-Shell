004da7f3fa7849555f74e29734d97ce0953388e4
Caio Cesar Ferreira Peixoto Machado <63310044+caiocfpeixoto@users.noreply.github.com>
2022-10-07 22:44:36 +0000

    shell finalizado

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void) {
-  char * argv[] = {"/bin/ls","-l",NULL};
+  pid_T pid;
+
+  char * argv[] = {"/bin/ls","-l",NULL}; // NULL para saber o fim da string
+
+  pid = fork();
+  if(pid == -1){
+    return -1;
+  }
+
+  if(pid == 0){ // PROCESSO FILHO
+    int teste = execve(argv[0], argv , NULL); // caso tenha erro retorna -1. Usando variável teste para saber o retorno
+    if( teste == -1 ){
+      perror("Erro,Deu ruim!");
+    }
+  }else{ // PROCESSO PAI
+    wait(NULL);
+    printf("Deu certo :)");
+  }
+  
+  
   return 0;
 }
